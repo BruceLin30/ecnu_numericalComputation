92f4aef27cd151faac9d26387fd552baac2f953c
BruceLin30 <1304840298@qq.com>
2021-11-15 19:34:38 +0800

    add'

--- code3/SolvingLinearEquations.java
+++ code3/SolvingLinearEquations.java
@@ -2,22 +2,29 @@ import java.applet.Applet;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import javax.swing.*;
 
 import java.awt.Graphics;
 
-public class NumericalIntegrating {
+public class SolvingLinearEquations {
 
-    static String strValueN = new String();
-    //代表文本框输入的步长
-    static String strValueABE = new String();
-    //代表文本框输入的参数，a,b,e
-    static int valueN;
-    //代表int值的n步长
-    static double valueA,valueB,valueE;
-    //代表double值的a，b，e
-    static int flag = 0;
-    //0表示没有e，1表示有e
+    //static String[] strA;
+    static String strb;
+    static ArrayList<String> strA = new ArrayList<String>();
+    static double [][]a;
+    static double []b;
+    static public int dim;
+    static String test1a = new String("4 2 -3 -1 2 1 0 0 0 0\n8 6 -5 -3 6 5 0 1 0 0\n4 2 -2 -1 3 2 -1 0 3 1\n0 -2 1 5 -1 3 -1 1 9 4\n-4 2 6 -1 6 7 -3 3 2 3\n8 6 -8 5 7 17 2 6 -3 5\n0 2 -1 3 -4 2 5 3 0 1\n16 10 -11 -9 17 34 2 -1 2 2\n4 6 2 -7 13 9 2 0 12 4\n0 0 -1 8 -3 -24 -8 6 3 -1");
+    static String test1b = new String("5 12 3 2 3 46 13 38 19 -21");
+    static String test2a = new String("4 2 -4 0 2 4 0 0\n2 2 -1 -2 1 3 2 0\n-4 -1 14 1 -8 -3 5 6\n0 -2 1 6 -1 -4 -3 3\n2 1 -8 -1 22 4 -10 -3\n4 3 -3 -4 4 11 1 -4\n0 2 5 -3 -10 1 14 2\n0 0 6 3 -3 -4 2 19");
+    static String test2b = new String("0 -6 20 23 9 -22 -15 45");
+    static String test3a = new String("4 -1 0 0 0 0 0 0 0 0\n-1 4 -1 0 0 0 0 0 0 0\n0 -1 4 -1 0 0 0 0 0 0\n0 0 -1 4 -1 0 0 0 0 0\n0 0 0 -1 4 -1 0 0 0 0\n0 0 0 0 -1 4 -1 0 0 0\n0 0 0 0 0 -1 4 -1 0 0\n0 0 0 0 0 0 -1 4 -1 0\n0 0 0 0 0 0 0 -1 4 -1\n0 0 0 0 0 0 0 0 -1 4");
+    static String test3b = new String("7 5 -13 2 6 -12 14 -4 5 -5");
+    static Fun fun = new Fun();
     static String FunType = new String("NULL");
     // 默认的初始模式是空模式
     static String ModeType = new String("NULL");
@@ -40,42 +47,53 @@ public class NumericalIntegrating {
     static JTextField jFieldResult = new JTextField(120);
     static JTextField jFieldResult2 = new JTextField(120);
     static JLabel labelImg = new JLabel();//载入函数图片的jlabel
-    static fun f = new fun();
+    //static fun f = new fun();
     public static void main(String[] args) {
         System.out.println("Test Success!");
-        NumericalIntegrating NI = new NumericalIntegrating();
+        SolvingLinearEquations NI = new SolvingLinearEquations();
 
         NI.initMenuBar();// 初始化菜单栏
         NI.initUI();// 初始化UI界面
+
         /*
-        fun f = new fun();
-        f.setData(0, 1, 20, 4);
-        f.setEpsilon(0.001);
-        System.out.println(f.calculateFun(1));
-        System.out.println(f.calculateFun(2));
-        System.out.println(f.calculateFun(3));
+        double ex1[][] = new double[3][3];
+        double ex2[] = new double[3];
+        int cnt = 1;
+        for (int i = 0;i < 3;i++)
+        {
+            for (int j = 0 ; j < 3;j++)
+            {
+                ex1[i][j] = cnt;
+                cnt ++;
+            }
+        }
+        for (int i = 0;i < 3;i++ )
+        {
+            ex2[i] = i+ 1;
+        }
+        Fun fun = new Fun();
+        fun.setData(ex2, ex1);
+        fun.printData();
         */
+        
+        
+
     }
-    public void processInput(String strABEN)
+    public void processInput(String sa,String sb)
     {
-        String [] str = strABEN.split(" ");
-        if (str.length == 1)//一个参数，只有N
+        String tmpb[] = sb.split(" ");
+        b = new double[tmpb.length];
+        a = new double[tmpb.length][tmpb.length];
+        for (int i = 0;i < tmpb.length;i++)
         {
-            valueN = Integer.valueOf(str[0]);
+            b[i] = Double.parseDouble(tmpb[i]);
         }
-        else if (str.length == 2)//两个参数，没有e,只有AB
+        String []tmpa = sa.split("\n|\\s+");
+        for (int i = 0 ; i < tmpa.length;i++)
         {
-            valueA = Double.parseDouble(str[0]);
-            valueB = Double.parseDouble(str[1]);
-            flag = 0;
+            strA.add(tmpa[i]);
         }
-        else if (str.length == 3)//三个参数ABE
-        {
-            valueA = Double.parseDouble(str[0]);
-            valueB = Double.parseDouble(str[1]);
-            valueE = Double.parseDouble(str[2]);
-            flag = 1;
-        }
+        dim = b.length;
     }
     /**
      * 更新结果的UI
@@ -136,13 +154,22 @@ public class NumericalIntegrating {
         /**
          * 这里是对labels的设置
          */
-        JLabel label = new JLabel("数值积分函数：");
-        label.setBounds(20, 20, 130, 20);
+        JLabel label = new JLabel("输入需要求解方程组的A：");
+        label.setBounds(20, 50, 130, 20);
         label.setForeground(Color.BLUE);
         p.add(label);
+
+        JLabel label1 = new JLabel("当前选择的方程组解法：");
+        label1.setBounds(20, 20, 200, 20);
+        label1.setForeground(Color.BLUE);
+        p.add(label1);
+
+         /*
         JLabel labelfun = new JLabel("数值积分公式：");
         labelfun.setBounds(20, 70, 130, 20);
         labelfun.setForeground(Color.BLUE);
+
+       
         p.add(labelfun);
         JLabel label2 = new JLabel("请输入积分上下限(请输入非分数)：");
         label2.setBounds(20, 120, 130, 20);
@@ -160,16 +187,21 @@ public class NumericalIntegrating {
         JLabel label5 = new JLabel("积分结果：");
         label5.setBounds(20, 240, 100, 20);
         p.add(label5);
-        /*
-        JLabel label6 = new JLabel("最佳步长：");
-        label6.setBounds(20,310,100,20);
+       */ 
+        JLabel label6 = new JLabel("请输入需要求解的方程组的B：");
+        label6.setBounds(20,310,200,20);
+        label6.setForeground(Color.BLUE);
         p.add(label6);
-        */
+        
 
         // frame.add(label);
         /**
          * JTextField的设置 创建文本框，指定可见列数为80列
          */
+
+         /*
+         
+         
         jFieldMode.setText("当前函数：未选择");
         jFieldMode.setEditable(false);
         jFieldMode.setBounds(250, 20, 200, 30);
@@ -185,108 +217,117 @@ public class NumericalIntegrating {
         final JTextField jFieldX = new JTextField(80);
         jFieldX.setBounds(250, 120, 200, 30);
         p.add(jFieldX);
-        final JTextField jFieldn = new JTextField(80);
-        jFieldn.setBounds(250, 180, 200, 30);
-        p.add(jFieldn);
-        jFieldResult = new JTextField(80);
-        jFieldResult.setEditable(false);
-        jFieldResult.setBounds(250, 240, 200, 30);
-        p.add(jFieldResult);
-
-        /*
-        jFieldResult2 = new JTextField(80);
-        jFieldResult2.setEditable(false);
-        jFieldResult2.setBounds(250, 300, 200, 30);
-        p.add(jFieldResult2);
         */
+        final JTextArea jarea = new JTextArea("请输入方程组的A",200,200);
+        jarea.setBounds(20, 90, 200, 200);
+        p.add(jarea);
+
+
+        final JTextField jFieldX = new JTextField(80);
+        jFieldX.setBounds(20, 350, 200, 30);
+        p.add(jFieldX);
+        
+        
+
+
         
         /**
          * 这里是对Buttons的设置
          */
         JButton button1 = new JButton("开始计算");//
         button1.setBounds(250, 350, 200, 40);// 设置按钮在容器中的位置
-
         p.add(button1);
+        
 
         button1.addActionListener(new ActionListener()// 对按钮增加监听
         {
             // 此处需要使用的是匿名类，需要重写actionPerformed函数，否则会出错
             @Override
             public void actionPerformed(ActionEvent e) {
-                //定义了两个临时的字符串存储数据
-                String tmpStrABE = new String("");
-                String tmpStrN = new String("");
-                tmpStrABE = jFieldX.getText();
-                tmpStrN = jFieldn.getText();
-                //以下是对输入异常的处理，出现异常则调用updateUI来提示用户
-                if (ModeTypeInt == 0 || FunTypeInt == 0)
+                //处理输入
+                //System.out.println(jarea.getText());
+                //System.out.println(jarea.getText().length());
+                for (int i = strA.size() - 1 ; i >= 0;i--)
                 {
-                    updateUI("err: 请选择函数和积分方法 !");
+                    strA.remove(i);
                 }
-                else if (tmpStrABE.compareTo("") == 0 || tmpStrN.compareTo("") == 0)
+                //因为是ArrayList，所以每次使用前需要清空
+                processInput(jarea.getText(),jFieldX.getText());
+                //将jArea中的字符串处理成字符串数组
+                //System.out.println(strA.size());
+                //for (int i = 0 ;i < strA.size();i++) System.out.println(strA.get(i));
+                int cnt = 0;
+                for (int i = 0 ; i< dim;i++)
                 {
-                    updateUI("err: 请输入参数!");
-                }
-                else if (tmpStrABE.split(" ").length == 1)
-                {
-                    updateUI("err: 请输入正确数量的参数 !");
-                }
-                //没有出现输入异常，就调用processInput函数处理输入
-                else
-                {
-                    processInput(tmpStrABE);
-                    processInput(tmpStrN);
-                    
-                    if (flag == 0)
+                    for (int j = 0; j < dim;j++)
                     {
-                        f.setData(valueA, valueB, valueN, FunTypeInt);
+                        a[i][j] = Double.parseDouble(strA.get(cnt++));
                     }
-                    else if (flag == 1)
-                    {
-                        f.setData(valueA, valueB, valueN, FunTypeInt);
-                        f.setEpsilon(valueE);
-                    }
-                    //调用f.calculate来实现结果的更新
-                    updateUI(f.calculateFun(ModeTypeInt));
                 }
+                
+                //测试输出
+                /*
+                for (int i = 0 ; i < dim;i++)
+                {
+                    for (int j = 0 ; j < dim;j++)
+                    {
+                        System.out.print(a[i][j] + " ");
+                    }
+                    System.out.println(" ");
+                }
+                */
+                fun.setData(b, a,dim);
+                fun.calGAussEWPP();
+                fun.printResult();
             }
         });
-        /*
-        JButton button2 = new JButton("确定最佳步长");//
-        button2.setBounds(20, 350, 200, 40);// 设置按钮在容器中的位置
 
+        
+
+       
+        JButton button2 = new JButton("测试样例1---线性方程组");//
+        button2.setBounds(20, 400, 200, 40);// 设置按钮在容器中的位置
         p.add(button2);
         button2.addActionListener(new ActionListener(){
 
             @Override
             public void actionPerformed(ActionEvent e) {
                 // TODO Auto-generated method stub
-                String tmpStrABE = new String("");
-                String tmpStrN = new String("");
-                tmpStrABE = jFieldX.getText();
-                tmpStrN = jFieldn.getText();
+                jarea.setText(test1a);
+                jFieldX.setText(test1b);
 
-                processInput(tmpStrABE);
-                processInput(tmpStrN);
-                if (flag == 0)
-                {
-                    f.setData(valueA, valueB, valueN, FunTypeInt);
-                }
-                else if (flag == 1)
-                {
-                    f.setData(valueA, valueB, valueN, FunTypeInt);
-                    f.setEpsilon(valueE);
-                }
-            
-                double [] resarr = new double[120];
-                for (int i = 2 ;i <= 100;i++)
-                {   
-                    f.setData(valueA, valueB, i,FunTypeInt);
-                    resarr[i - 2] = f.calculateFun(ModeTypeInt);
-                }
             }
         });
-        */
+
+        JButton button3 = new JButton("测试样例2---对称正定线性方程组");
+        button3.setBounds(20, 440, 200, 40);
+        p.add(button3);
+        button3.addActionListener(new ActionListener(){
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // TODO Auto-generated method stub
+                jarea.setText(test2a);
+                jFieldX.setText(test2b);
+
+            }
+        });
+
+        JButton button4 = new JButton("测试样例2---对称正定线性方程组");
+        button4.setBounds(20, 480, 200, 40);
+        p.add(button4);
+        button4.addActionListener(new ActionListener(){
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // TODO Auto-generated method stub
+                jarea.setText(test3a);
+                jFieldX.setText(test3b);
+
+            }
+        });
+
+        
         
         /**
          * 这里是函数结尾的必要设置
@@ -443,103 +484,132 @@ public class NumericalIntegrating {
         }
     }
 }
-
-class fun{
-    //定义了积分的上下限，步长，n和e
-    public double a, b, h,epsilon;
-    public int n;
-    //selectedFun表示被选定的函数的类型
-    public int selectedFun;
-    //设置类内参数的a，b，n，和用来表示函数类型的int值
-    public void setData(double aa, double bb, int nn, int f) {
-        a = aa;
-        b = bb;
-        n = nn;
-        h = (b - a) / n;
-        selectedFun = f;
-    }
-    //定义了类内参数的e
-    public void setEpsilon(double e)
+class Fun
+{
+    public int dimension;
+    public double [][]A;
+    public double []B;
+    public double []X;
+    public void setData(double b[],double a[][],int dim)
     {
-        epsilon = e;
-    }
-    /**
-     * @param x
-     * @param num 1表示第一个函数，2表示第二个函数，以此类推
-     * @return 计算出的类型为num的函数值
-     */
-    private double calculate(double x, int num) {
-        //计算函数值，x代表x点，num代表函数的类型
-        if (num == 1)
-            return Math.sqrt(4 - (Math.sin(x)) * (Math.sin(x)));
-        if (num == 2)
+        dimension = dim;
+        //System.out.println(dimension);
+        //System.out.println(123213123);
+        A = new double[dimension + 2][dimension + 2];
+        B = new double[dimension + 2];
+        X = new double[dimension + 2];
+        for (int i = 1;i <= dimension ;i++)
         {
-            if (x == 0)
-                return (double)(Math.cos(x));//洛必达法则，0/0型
-            else 
-                return (double) (Math.sin(x) / x);
+            B[i] = b[i - 1];
         }
-        if (num == 3)
-            return (double) ((Math.pow(Math.E, x)) / (4 + x * x));
-        if (num == 4)
-            return (double) (Math.log(1 + x) / (1 + x * x));
-        return -1;
+        for (int i = 1;i <= dimension;i++)
+        {
+            for (int j = 1; j <= dimension;j++)
+            {
+                A[i][j] = a[i - 1][j - 1];
+            }
+        }
     }
-
+    public void printData()
+    {
+        for (int i = 0;i < dimension;i++)
+        {
+            System.out.print(B[i] + " ");
+        }
+        System.out.println("");
+        for (int i = 0 ; i < dimension;i++)
+        {
+            for (int j = 0 ; j < dimension;j++)
+            {
+                System.out.print(A[i][j] + " ");
+            }
+            System.out.println("");
+        }
+    }
+    public void printResult()
+    {
+        for (int i = 1 ; i <= dimension;i++)
+        {
+            System.out.print(Math.round(B[i]) + " ");
+        }
+        System.out.println("");
+        for (int i = 1;i <= dimension;i++)
+        {
+            System.out.print(B[i] + " ");
+        }
+        System.out.println("");
+    }
     /**
-     * modetype == 1 复合梯形
-     * modetype == 2 simpson
-     * modetype == 3 Romberg
+     * 高斯顺序消去法
+     * @return B
+     */
+    public double[] calGauss()
+    {
+        for (int k = 1;k <= dimension - 1;k++)
+        {
+            for (int i = k + 1;i <= dimension;i++)
+            {
+                A[i][k] =  A[i][k] / A[k][k];
+                for (int j = k + 1;j <= dimension;j++)
+                {
+                    A[i][j] = A[i][j] - A[i][k] * A[k][j];
+                }
+                B[i] = B[i] - A[i][k] * B[k];
+            }
+        }
+        for (int i = dimension;i >= 1;i--)
+        {
+            for (int j = i + 1;j <= dimension;j++)
+                B[i] = B[i] - A[i][j] * B[j];
+            B[i] = B[i] / A[i][i];
+        }
+        return B;
+    }
+    /**
+     * 高斯列主元消元法
      * @return
      */
-    public double calculateFun(int modetype) {
-        if (modetype == 1)
+    public double[] calGAussEWPP()
+    {
+        int k;
+        double t;
+        for (int i = 1; i <= dimension;i++)
         {
-            double res = 0;
-            double cur = a;
-            int times = n;
-            while (times > 0) {
-                res += h / 2 * (calculate(cur, selectedFun) + calculate(cur + h, selectedFun));
-                times--;
-                cur += h;
+            k = i;
+            for (int j = i + 1;j <= dimension;j++)
+            {
+                if (Math.abs(A[k][i]) < Math.abs(A[k][j]))
+                {
+                    k = j;
+                }
             }
-            return res;
+            for (int j = i;j <= dimension;j++)
+            {
+                t = A[i][j];
+                A[i][j] = A[k][j];
+                A[k][j] = t;
+            }
+            t = B[i];
+            B[i] = B[k];
+            B[k] = t;
+            for (int j = i + 1;j <= dimension;j++)
+            {
+                A[j][i] = A[j][i] / A[i][i];
+                for (k = i + 1;k <= dimension;k++)
+                {
+                    A[j][k] = A[j][k] - A[j][i] * A[i][k];
+                }
+                B[j] = B[j] - A[j][i] * B[i];
+            }
         }
-        if (modetype == 2)//如果是
+        for (int i = dimension;i >= 1;i--)
         {
-            double res = 0;
-            double cur = a;
-            int times = n / 2;
-            while (times > 0)
+            for (int j = i + 1;j <= dimension;j++)
             {
-                res += h / 3 * (calculate(cur, selectedFun) + 4 * calculate(cur + h, selectedFun) + calculate(cur + 2 * h, selectedFun));
-                times--;
-                cur += 2 * h;
+                B[i] = B[i] - A[i][j]*B[j];
             }
-            return res;
+            B[i] = B[i] / A[i][i];
         }
-        if (modetype == 3)
-        {
-            int m = 1, k = 1;
-            double hh = (b - a) / 2.0;
-            double T0 = hh * (calculate(a,selectedFun) + calculate(b,selectedFun)), T = 3;
-            double F = 0;
-            while(Math.abs(T - T0) >= 3 * epsilon)
-            {
-            if(m != 1)
-                T0 = T;
-            F = 0;
-            k = (int) Math.pow(2., m - 1);
-            for(int i = 1; i <= k ; i++)
-            {
-                F += calculate(a + (2 * i - 1) * hh , selectedFun);
-            }
-            T = T0 / 2.0 + hh * F;
-            m += 1;
-            hh /= 2.0;  	
-            }
-            return T;
-        }
-        return -1;
+        return B;
     }
 }
